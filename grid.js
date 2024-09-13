124ab0da21fd5d0387d7679e57ff67b1ceef9e1c
Asher-zx <zhixuan.wang@hyperisland.se>
2024-09-13 23:30:59 +0200

    hover effect on grid

--- grid.js
+++ grid.js
@@ -1,21 +1,36 @@
 // select the grid
 const gridContainer = document.getElementById('gridcontainer');
 
+// set the background color
+const multipleColor = "black";
+
 // create a grid of squares
-function createGridItems(rows, cols) {
+function createGrid(rows, cols) {
   for (let i =0; i < rows * cols; i++) {
     // create a new div
     const gridItem = document.createElement('div');
+
     gridItem.className = 'grid-item';
     // add class
 
-    gridItem.textContent = i + 1;
-    // set the content of the div
+    // add eventlistener
+    gridItem.addEventListener('mouseover', () => {
+      gridItem.style.background = 'linear-gradient(45deg, #ff6b6b, #f7e84c)'; // change to gradient
+    })
+
+    //add event listener for mouseout to reset color
+    gridItem.addEventListener('mouseout', () => {
+      gridItem.style.background = '#6ccbea'; //Reset to defaultcolor
+    })
 
     //append the new div to the grid container
     gridContainer.appendChild(gridItem);
+
+ 
   };
 }
 
+
+
 // create 16 x 16 grid items
-createGridItems(16, 16);
+createGrid(16, 16);
